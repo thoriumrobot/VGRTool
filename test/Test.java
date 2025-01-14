4a7c2e79f9033d43b4ce597536bd9246e0304a47
thoriumrobot <ks225@njit.edu>
2025-01-14 10:18:41 -0500

    reset test

--- test/Test.java
+++ test/Test.java
@@ -1,14 +1,11 @@
 class Test {
 	void test() {
-				Class<?> handlerType = (handlerMethod != null ? handlerMethod.getBeanType() : null);
+		Class<?> handlerType = (handlerMethod != null ? handlerMethod.getBeanType() : null);
 		Object exceptionHandlerObject = null;
 		Method exceptionHandlerMethod = null;
 
 		if (handlerType != null) {
-			{
-				Objects.requireNonNull(handlerMethod);
-				exceptionHandlerObject = handlerMethod.getBean();
-			}
+			exceptionHandlerObject = handlerMethod.getBean();
 		}
 	}
 }
